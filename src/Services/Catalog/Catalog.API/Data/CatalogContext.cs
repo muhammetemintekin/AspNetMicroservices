366ca858d068708b6b21503703625054cfe1a74e
Muhammet Emin Tekin <muhammetemintekin@yandex.com>
2022-03-18 22:53:03 +0300

    committed

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,17 @@
+using MongoDB.Driver;
+
+public class CatalogContext : ICatalogContext
+{
+    public CatalogContext(IConfiguration configuration)
+    {
+        var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+        var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+
+        Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+        //CatalogContextSeed.SeedData(Products);
+    }
+
+    public IMongoCollection<Product> Products { get; }
+
+    IMongoCollection<Product> ICatalogContext.Products => throw new NotImplementedException();
+}
